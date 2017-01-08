7e8f9c059886218b62833eccd53214ca60b41a34
ChanderG <chanderg@chandergovind.org>
2017-01-08 10:42:48 +0530

    move to darker blue for the bar and border

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
+static const char selbordercolor[]  = "#003366";
+static const char selbgcolor[]      = "#003366";
 static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
